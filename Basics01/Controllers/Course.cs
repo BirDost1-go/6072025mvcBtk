2a11ca895aef12a2ab7f9a9f5ea5fc462c3128c2
muham <muham@MUHAMMET>
2025-07-06 23:18:31 +0300

    Add project files.

---
+++ Basics01/Controllers/Course.cs
@@ -0,0 +1,36 @@
+﻿using Basics01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics01.Controllers
+{
+    public class Course : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = Repository.Applications;
+            return View(model);
+        }
+        public IActionResult Apply()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate model)
+        {
+
+            if (Repository.Applications.Any(c => c.Email.Equals(model.Email)))
+            {
+                ModelState.AddModelError("", "there is already an application for this email");
+            }
+
+            if (ModelState.IsValid)
+            {
+                Repository.Add(model);
+                return View("Feedback", model);
+            }
+            return View();
+
+        }
+    }
+}
---
+++ Basics01/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Basics01.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basics01.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult IndexE()
+        {
+            string message = $"hello world. {DateTime.Now.ToString()}";
+            return View("IndexE",message);
+            
+        }
+        public string IndexE2()
+        {
+            return "ayberk";
+
+        }
+        public ViewResult IndexE3()
+        {
+            var list = new List<Employee>()
+            {
+                new Employee(){Id=1,FirstName="abdf",LastName="aytar`",Age=20 },
+                new Employee(){Id=2,FirstName="b",LastName="aytar`",Age=3 },
+                new Employee(){Id=3,FirstName="n",LastName="aytar`",Age=4 },
+            };
+            return View("IndexE2",list);
+        }
+
+
+    }
+}  
